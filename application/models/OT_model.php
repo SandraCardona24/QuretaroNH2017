2328f940229e82d10d9d177efa5e212da703de1e
pjuanluis <pjuanluis97@gmail.com>
2017-10-06 19:04:10 -0500

    sE CREA CONTROLADORES Y METODOS PARA ORDENES DE TRABAJO

---
+++ application/models/OT_model.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class OT_model extends CI_Model{
+	
+	
+	public function __construct(){
+		$this->load->database();
+	}
+
+	public function insertOT($datos) {
+
+		$this->db->insert("ORDEN_TRABAJO", $datos);
+	}
+
+	public function selectOTs(){
+
+		$this->db->select("ID_OT, DESC_OT, HORAS_OT, FEC_IN_OT, FEC_FN_OT, DESC_ESTATUS, LIDER_CLIENTE_OT");
+		$this->db->from("ORDEN_TRABAJO");
+		$this->db->join("ESTATUS","ORDEN_TRABAJO.ID_ESTATUS = ESTATUS.ID_ESTATUS");
+
+		$query = $this->db->get();
+		return ($query->num_rows() <= 0) ? NULL : $query->result(); 
+	}
+
+	public function selectEstatus(){
+
+		$this->db->select("*");
+		$this->db->from("ESTATUS");
+		$this->db->where("TIPO_ESTATUS", "ot");
+		$query = $this->db->get();
+		return ($query->num_rows() <= 0) ? NULL : $query->result(); 
+	}
+
+	public function updateOT($id, $datos) {
+
+		$this->db->where("ID_OT", $id);
+		$this->db->update("ORDEN_TRABAJO", $datos);
+
+	}
+
+	public function deleteOT($id){
+
+		$this->db->where("ID_ESTATUS", $id);
+		$this->db->update("ORDEN_TRABAJO", 10);
+	}
+
+}
---
+++ application/views/ot_view.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<form action="insertOrden" method="post">
+			<label>Desc</label>
+			<input type="text" name="desc"><br>
+			<label>Horas</label>
+			<input type="text" name="horas"><br>
+			<label>Fecha inicio</label>
+			<input type="text" name="fec_in"><br>
+			<small>Fecha fin</small>
+			<input type="text" name="fec_fin"><br>
+			<label>Estatus</label>
+			<select name="id_estatus">
+				<option value="0">Seleccione</option>
+				<?php foreach ($estatus['estatus'] as $e) { ?>
+				<option value="<?=$e->ID_ESTATUS; ?>"><?=$e->DESC_ESTATUS; ?></option>					
+				<?php } ?>
+			</select><br>
+			<label>Lider</label>
+			<input type="text" name="lider"><br>
+			<input type="submit" name="Aceptar">
+		</form>
+	</div>
+	<br><br>
+	<div>
+		<table>			
+			<tr>
+				<th>ID</th>
+				<th>DESCRIPCION</th>
+				<th>HORAS</th>
+				<th>FECHA INICIO</th>
+				<th>FECHA FIN</th>
+				<th>ESTATUS</th>
+				<th>LIDER</th>
+			</tr>			
+			<?php foreach ($ordenes['ordenes'] as $d) { ?>
+				<tr>
+					<td><?=$d->ID_OT; ?></td>
+					<td><?=$d->DESC_OT; ?></td>
+					<td><?=$d->HORAS_OT; ?></td>
+					<td><?=$d->FEC_IN_OT; ?></td>
+					<td><?=$d->FEC_FN_OT; ?></td>
+					<td><?=$d->DESC_ESTATUS; ?></td>
+					<td><?=$d->LIDER_CLIENTE_OT; ?></td>
+				</tr>				
+			<?php } ?>				
+			
+		</table>
+
+	</div>
+
+</body>
+</html>
